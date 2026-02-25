de58955e8861d8e57990cab2ef35ae8288f2f6a1
Armando R <armando.ramos@uttn.mx>
2026-02-25 16:55:57 -0600

    Switch to capacitor.config.json to avoid TypeScript dependency

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.daniel.clima',
-  appName: 'clima-app',
-  webDir: 'build'
-};
-
-export default config;
